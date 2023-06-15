6e66625090431ac835c91c40007a540e0a71e930
Nathalia <nathalia.cantarelli@edu.pucrs.br>
2023-06-14 23:12:40 -0300

    Requisito 5

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -2,17 +2,21 @@ import sys
 from ting_file_management.file_management import txt_importer
 
 
-def process(path_file, instance):
-    if path_file in instance._data:
-        return None
+def out_file(path_file):
     file = txt_importer(path_file)
     out_process = {
         "nome_do_arquivo": path_file,
         "qtd_linhas": len(file),
         "linhas_do_arquivo": file,
     }
-    instance.enqueue(path_file)
     print(str(out_process))
+
+
+def process(path_file, instance):
+    if path_file in instance._data:
+        return None
+    out_file(path_file=path_file)
+    instance.enqueue(path_file)
 
 
 def remove(instance):
@@ -23,4 +27,8 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        path_file = instance.search(position)
+        out_file(path_file=path_file)
+    except IndexError:
+        sys.stderr.write("Posição inválida")
