89c27aba7fd93f63f5f2635ee003e6664c88f5d4
Nathalia <nathalia.cantarelli@edu.pucrs.br>
2023-06-14 22:56:43 -0300

    Requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,18 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if path_file in instance._data:
+        return None
+    file = txt_importer(path_file)
+    out_process = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file,
+    }
+    instance.enqueue(path_file)
+    print(str(out_process))
 
 
 def remove(instance):
