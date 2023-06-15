bfc0b8114f8b29fe2d0e4ea8852ee86c42a20c90
Nathalia <nathalia.cantarelli@edu.pucrs.br>
2023-06-14 23:39:26 -0300

    Requisito 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,26 @@
+from ting_file_management.file_management import txt_importer
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    files_with_word = []
+    for file in instance._data:
+        content = txt_importer(file)
+        lines_with_word = [
+            {"linha": index + 1}
+            for index, line in enumerate(content)
+            if word.lower() in line.lower()
+        ]
+
+        if lines_with_word:
+            files_with_word.append(
+                {
+                    "palavra": word,
+                    "arquivo": file,
+                    "ocorrencias": lines_with_word,
+                }
+            )
+
+    return files_with_word
 
 
 def search_by_word(word, instance):
