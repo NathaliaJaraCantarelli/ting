e70bef5da25bd9e78c3252284259d325cc5206ca
Nathalia <nathalia.cantarelli@edu.pucrs.br>
2023-06-14 22:38:46 -0300

    Requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,17 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    content = []
+    try:
+        if not path_file.endswith('.txt'):
+            sys.stderr.write('Formato inválido')
+
+        with open(path_file, 'r') as file:
+            for line in file:
+                content.append(line.strip())
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+
+    return content
